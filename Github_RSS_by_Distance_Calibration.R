2b67d8a09fba532ecd87a447c7c78a16541ed3a5
Kristina Paxton <72826644+kpaxton75@users.noreply.github.com>
2022-01-11 13:41:03 -1000

    Add files via upload

---
+++ Github_RSS_by_Distance_Calibration.R
@@ -0,0 +1,230 @@
+###########################################################################################################################################################
+##
+##  Kristina L Paxton
+##
+##  April 1 2021
+##
+##    Code to examine the relationship between RSS values and distance for a given study area using an exponential decay model
+##      -- Based on data from a test dataset of a radio transmitter at 135 known locations distributed throughout the Guam Network 
+##         -- At each test location a transmitter was held stationary for a 5-minute time period 
+##            -- Removed first and last minute of each test to ensure that times matched between tests and the node network
+##            -- For each test, calculated an average RSS value for the 3-min middle time period individually for each node that detected the transmitter
+##
+##
+##    1st step: Data preparation - isolates raw RSS data from node network that is associated with test data - creates the published data file - RSS.Localization.PaperDataset.csv
+##    2nd step: Exponential Decay Model - uses the dataset created in step 1 to examine the relationship between RSS values and distance
+##
+##
+##    Files Needed
+##        1. TestInfo.csv: file with test information that is saved in the working directory defined below
+##            - For each unique test location there should a row of information associated with each of the 3 inner minutes of that test
+##                - meaning that each test will have 3 rows of information with each row identifying one of the 3 inner minutes of the test (see TestInfo_Example.csv)
+##            - Columns
+##                -- TagId: unique identifier of the transmitter used during the specified test
+##                -- TestId: unique identifier given to each of the unique locations where a test was conducted
+##                -- Date: date when the specified test was conducted
+##                -- Start.Time: time when the specified test was started
+##                -- End.Time: time when the specified test was ended
+##                -- Min: 1-minute time period of the specified test
+##                -- Hour: hour of the the specified minute of the test
+##                -- TestUTMx: Easting location of the specified test 
+##                -- TestUTMy: Northing location of the specified test 
+##
+##       2. BeepData.rds: file with the raw RSS values collected by the node network during the time period of the test. The file should be saved in the working directory defined below
+##            -- output file from Import_beep.data.Github.R script (see BeepData_Example.rds)
+##            -- If using a file created from another source the following columns are needed in the specified format: 
+##                -- TagId: Factor identifying the unique code of a tag
+##                -- Time.local: POSIXct value in the format: "2021-12-29 09:02:51" that identifies the unique datetime stamp (IN THE LOCAL TIME ZONE OF THE STUDY) when the tag was detected by the specified node
+##                    ***** If you don't have a column with the local time, but only UTC time - then change lines 42-44 in Functions_RSS.Based.Localizations.R from 'Time.local' to 'Time' ************
+##                    ***** BUT be sure that Dates and Times in TestInfo.csv are also in UTC time and not local time *********
+##                -- NodeId: Factor identifying the node in the network that detected the specified tag
+##                -- TagRSSI: Integer indicating the RSS value (in dB) of the signal of the specified tag received by the specified node
+##
+##       3. Nodes.csv: file with a list of all nodes in your network and their UTM locations. The file should be saved in the working directory defined below
+##            - Columns
+##                -- NodeId: unique identifier of given to each node 
+#                 -- NodeUTMx: Easting location of the specified node
+##                -- NodeUTMy: Northing location of the specified node 
+##            - Other columns can also be in the file for your reference 
+##            - If Node names are being converted to scientific notation in Excel open the file with a text editor (e.g. BBedit) to change names to the correct format and save the file  
+## 
+##       4. Functions_RSS.Based.Localizations.R - R script with functions needed to run the code below - file should be saved in the working directory defined below    
+##      
+##  Important Output
+##      1. Calibration Dataset that contains the average RSS values for a given node that detected the signal of a test transmitter at a known location
+##      2. Model coefficients from an exponential decay model that show the relationship between RSS and Distance for a node network
+##            exponential model formula: avgRSS ~ a * exp(-S * distance) + K
+##                  a = intercept
+##                  S = decay factor
+##                  K = horizontal asymptote
+##
+##  
+##########################################################################################################################################################
+
+
+
+# Packages needed
+library(dplyr)
+library(lubridate)
+library(ggplot2)
+
+# Reset R's brain - removes all previous objects
+rm(list=ls())
+
+## Set by User
+# Working Directory - Provide/path/on/your/computer/where/master/csv/file/of/nodes/is/found/and/where/Functions_CTT.Network.R/is/located
+working.directory <- " add here "
+
+# Directory for Output data - Provide/path/where/you/want/output/data/to/be/stored/
+outpath <- " add here - make sure it ends with a /"
+
+
+
+## Bring in functions 
+setwd(working.directory)
+source("Functions_RSS.Based.Localizations.R")
+
+
+
+## Bring in 3 Needed files - Test Information, RSS values, and Node Information - change file names in " " as needed
+test.info <- read.csv("Test.Info.csv", header = T)
+str(test.info) # check that data imported properly
+
+beep.dat <- readRDS("BeepData.rds") 
+str(beep.dat) # check that data imported properly
+
+nodes <- read.csv("Nodes.csv", header = T)
+str(nodes)
+
+
+
+
+
+#################################################################
+#   Step 1
+# Run function to isolate raw RSS data from a node network that
+# is associated with test data  
+#################################################################
+
+    ## Variables to define for function
+        ## DATE.FORMAT = format of the date column in Test.Info.csv file using R standard date expressions (e.g., "%m-%d-%y", "%Y-%m-%d")
+        ## TIMEZONE = Time zone where data was collected, use grep("<insert location name here>", OlsonNames(), value=TRUE) to find valid time zone name
+
+
+    ## Output in R environment
+        ## combined.data - dataframe that contains the average RSS values for a given node associated with each unique test
+
+
+    ## Output saved
+        ## RSS.Localization.PaperDataset.csv - same dataframe saved in the R environment is also saved in the folder specified by the outpath
+
+
+##******* Define Variables - replace values below with user specified values *******## 
+DATE.FORMAT <- "%m/%d/%y"
+TIME.ZONE <- "Pacific/Guam"
+
+
+# Combine RSS data from a node network with test information into a dataframe
+combined.data <- data.setup(DATE.FORMAT, TIME.ZONE)
+
+
+
+
+
+##########################################################
+#    Step 2
+# Exponential Decay Function to Examine Relationship 
+# between distance and Tag RSS values 
+##########################################################
+
+# Use the dataset created above or bring in a file with the data
+#combined.data <- read.csv("RSS.Localization.PaperDataset.csv")
+
+
+## Visualize data
+
+  # Plot of the relationship between RSS and distance
+ggplot(data = combined.data, aes(x = distance, y = avgRSS, color = NodeId)) +
+  geom_point(size = 2)
+
+
+
+## Preliminary Exponential Decay Model to determine starting values for the final model 
+
+    # SSasymp - self start for exponential model to finding the starting values of the data
+    # Asym - horizontal asymptote (when large values ) - y values decay to this value 
+    # R0 - numeric value when avgRSS (i.e., response variable) = 0 
+    # lrc - natural logarithm of the rate constant (rate of decay)
+
+
+  # Preliminary Model
+exp.mod <- nls(avgRSS ~ SSasymp(distance, Asym, R0, lrc), data = combined.data)
+  # Summary of Model
+summary(exp.mod)
+  # rate of decay
+exp(coef(exp.mod)[["lrc"]])
+
+
+
+
+## Exponential Decay Final Model with user provided self-starting values 
+## based on visualization of the data and values in the Preliminary Model Output
+
+    # exponential model formula: avgRSS ~ a * exp(-S * distance) + K
+      # a = intercept
+      # S = decay factor
+      # K = horizontal asymptote
+
+
+##  ***** Variables to define for final model below - replace values below with values from exp.mod ****  ## 
+a <- -59
+S <- 0.004561831
+K <- -104.63033
+
+  
+  # Final Model
+nls.mod <- nls(avgRSS ~ a * exp(-S * distance) + K, start = list(a = a, S = S, K= K), 
+               data = combined.data)
+  # Model Summary
+summary(nls.mod)
+  # Model Coefficients - *you will use these coefficient values to estimate distance based on RSS value in Github_Simulations.R*
+coef(nls.mod)
+
+
+## Check the fit of the model and get predicted values
+
+  # Get residuals and fit of model and add variables to main table
+combined.data$E <- residuals(nls.mod)
+combined.data$fit <- fitted(nls.mod)
+
+  # Plot residuals by fit or distance
+ggplot(combined.data, aes(x = distance, y = E, color = NodeId)) +
+         geom_point(size = 2)
+ggplot(combined.data, aes(x = fit, y = E, color = NodeId)) +
+  geom_point(size = 2)
+
+  # Get model predictions
+combined.data$pred <- predict(nls.mod)
+
+
+## Save Final Dataset with Residuals and Predictions
+write.csv(combined.data, paste0(outpath, "RSS.Localization.PaperDataset_withResiduals_Predictions.csv"),
+          row.names = F)
+
+
+## Plot with predicted line
+
+pdf(paste0(outpath, "Relationship_Distance~RSSI.pdf"), width = 8, height = 6)
+
+ggplot(combined.data, aes(x = distance, y = avgRSS)) + 
+  geom_point() +
+  geom_line(aes(y = pred), color = "#689FBB", lwd = 1.25) +
+  scale_y_continuous(name = "RSS (dB)") +
+  scale_x_continuous(name = "Distance (m)") +
+  theme_classic()
+
+dev.off()
+
+
+
+
---
+++ Github_Simulations.R
@@ -0,0 +1,260 @@
+###########################################################################################################################################################
+##
+##  Kristina L Paxton
+##
+##  April 1 2021
+##
+##    -- Code to simulate 3 node configurations within a 12.5 km2 grid - node spacing: 100m (n = 100 nodes), 175m (n = 64 nodes), or 250m (n = 36 nodes)
+##    -- Create simulated dataset of RSS values for a given node configuration incorporating variability in RSS values based on noise in your node network
+##    -- Assess the error associated with RSS-based localization estimates when filters are applied prior to trilateration analysis 
+##        -- localization error = difference in distance (m) between true and estimated location
+##      
+##
+##    Files Needed
+##        1. RSS.Localization.PaperDataset.csv that is saved in the working directory defined below
+##            - Test dataset used to calibrate RSS by Distance relationship
+##            - File was created in Github_RSS_by_Distance_Calibration.R script 
+##            - File MUST have the two following olumns:
+##                -- avgRSS: average RSS value for a 1-min time period for the specified node during the specified test
+##                -- distance: true distance between the specified node and specified test
+##
+##        2. Functions_RSS.Based.Localizations.R - R script with functions needed to run the code below - file should be saved in the working directory defined below    
+##      
+##    Important Output
+##        1. Dataset of simulated RSS values for a given node configuration
+##        2. Dataframes with summary statistics of localization error when different filters (No Filter, RSS filters, Distance filters) 
+##           are applied prior to trilateration analysis
+##  
+##########################################################################################################################################################
+
+
+# Packages needed
+library(tidyverse)
+library(raster)
+library(nlstools)
+
+
+# Reset R's brain - removes all previous objects
+rm(list=ls())
+
+
+## Set by User
+# Working Directory - Provide/path/on/your/computer/where/master/csv/file/of/nodes/is/found/and/where/Functions_CTT.Network.R/is/located
+working.directory <- " add here "
+
+# Directory for Output data - Provide/path/where/you/want/output/data/to/be/stored/
+outpath <- " add here - make sure it ends with a /"
+
+
+## Bring in functions 
+setwd(working.directory)
+source("Functions_RSS.Based.Localizations.R")
+
+
+## Bring Needed File - RSS by Distance Calibration Data - - change file name in " " as needed
+combined.data <- read.csv("RSS.Localization.PaperDataset.csv")
+
+
+
+
+
+###########################################################################
+# Create Grid of Nodes with Specified Spacing & Generate 100 Random Points
+##########################################################################
+
+    ## Variables to define for function
+        ## SIZE = spacing of the nodes - options: "n100", "n175", "n250"
+
+    ## Output in R environment
+        ## nodes = dataframe containing x and y locations of nodes with the designated spacing within a 12.5 km2 grid
+        ## random.points = dataframe with x and y locations of 100 random points within the node configuration
+        ## DIST.FILTER = vector with the distances that are a multiple (x1.25, x2, x3, x4) of the average grid spacing indicated
+        ## plot of nodes (black circles) and random points (grey triangles) for the designated spacing
+
+
+##******** Define Variable - replace value below with "n100", "n175", or "n250" ********##
+SIZE <- "n250"
+
+
+# Create node configuration and random points
+create.node.config(SIZE)
+
+
+
+
+
+##################################################################################################################################
+# Function to simulate RSS values for a given node configuration incorporating variability based on noise in your node network
+# Step 1: Use the true distance between a random point and a node to randomly select an RSS value
+#         associated with that distance in the RSS by Distance Calibration Dataset
+# Step 2: Estimate the distance between the random point and node based on the randomly selected RSS value 
+#         and the relationship between RSS values and distance determined based on an exponential decay function 
+################################################################################################################################
+
+    ## Variables to define for function  
+        ## NUM.SIM = number of simulations to run (recommend between 100 to 1,000)
+            # Simulations in the paper were based on 1,000 - the more simulations the longer time to generate data, but the more robust results
+        ## Coefficients in the exponential model: use output from Github_RSS_by_Distance_Calibration (e.g., coef(nls.mod))
+            ## a = intercept
+            ## S = decay factor
+            ## K = horizontal asymptote
+  
+
+    ## Output in Console
+        ## Iteration of the simulation will be printed at the end of each cycle to help you keep track of how fast the simulation is running
+
+    ## Output in R environment
+        ## random.rssi_results.all.sim - dataframe that contains the simulated values that will be needed for trilateration analysis
+            ## Columns
+                  ## NodeId - unique identifier of the specified node
+                  ## PointId - random point id of the specified test location
+                  ## t.dist - true distance between the specified node and test location
+                  ## r.rss - random RSS value selected from the dataset for the given true distance
+                  ## n.rss - number of potential RSS values for the given true distance
+                  ## sd.rss - standard deviation of the RSS values for the given true distance
+                  ## e.dist - estimated distance between the specified node and test location based on the random RSS value selected (e.g., relationship between RSS values and distance) 
+                  ## SimId - unique identifier given to each simulation
+                  ## x - location on the x-axis of the random point in the 12.5 km2 grid
+                  ## y - location on the y-axis of the random point in the 12.5 km2 grid
+                  ## TestId - unique identifier indicating the random point location and the simulation
+
+    ## Output saved
+        ## Simulated.rss.values_ - same dataframe in R environment save as an .rds file in your outpath with the spacing of the nodes 
+        ##                          indicated at the end of the name (e.g., Simulated.rss.values_250m.rds)
+
+
+##******** Define Variables - replace values below with user specified values ********##  
+NUM.SIM <- 100
+a <- 47.23 
+S <- 0.005
+K <- -105.16
+
+
+# Create simulated dataset to be used for trilateration analysis
+random.rss_results.all.sim <- get.RSS.values(NUM.SIM, a, S, K)
+
+
+
+
+
+
+########################################################################################
+# Trilateration - No filter
+    # Function that will estimate the location of each random point in the
+    # simulated dataset when no filters are applied prior to trilateration anaysis
+#######################################################################################
+
+    ## Output in Console
+        ## Iteration of the simulation will be printed at the end of each cycle to help you keep track of how fast the simulation is running
+            ## Total number of potential location estimates = 100 random points x NUM.SIM 
+
+    ## Output in R environment
+        ## no.filters - dataframe that contains the overall localization error when no filters are applied prior to trilateration 
+            ## Columns
+                ## n.pts - number of random points a location estimate could be calculated based on trilateration 
+                ## avg.no.sim - average number of simulations for each random point when a location estimate could be calculated based on trilateration 
+                ## avg.no.nodes - average number of nodes used in each trilateration analysis
+                ## avg.diff - average localization error of trilateration estimates when the specified filter was applied
+                ##            localization error = difference in distance (m) between true and estimated location
+                ## sd.diff - standard deviation of localization error of trilateration estimates when the specified filter was applied
+                ## lower.ci - lower 95% confidence interval of localization error of trilateration estimates when the specified filter was applied
+                ## upper.ci - upper 95% confidence interval of localization error of trilateration estimates when the specified filter was applied
+                ## med.diff - median localization error of trilateration estimates when the specified filter was applied
+                ## min.diff - minimum localization error of trilateration estimates when the specified filter was applied
+                ## max.dist - maximum localization error of trilateration estimates when the specified filter was applied
+                ## filter - filter applied prior to trilateration analysis
+
+    ## Output saved
+        ## Trilateration.Simulation_NoFilters_Summary.Stats.csv - .csv file of the same dataframe returned in the R environment 
+        ## Trilateration.Simulation_NoFilter_Summary.Results.csv - dataframe that has the summary statistics of localization error of when no filter  
+                                                                   ## was applied prior to trilateration. Each row represents the summary statistics 
+                                                                   ## for all simulations for a given random point. Files are for your reference 
+
+
+
+
+# Estimate locations of 100 random points when no filters are applied prior to trilateration 
+no.filters <- trilateration.NoFilter(random.rss_results.all.sim)
+
+
+ 
+
+
+
+#####################################################################################
+# Trilateration - RSS filters
+    # Function that will estimate the location of each random point
+    # in the simulated dataset when RSS filters are applied prior to trilateration 
+######################################################################################
+
+    ## Variable to define for function 
+        ## RSS.FILTER = RSS values that will be used to filter data prior to trilateration analysis
+                        ## All RSS values below the specified value will be removed from the simulated data
+
+
+    ## Output in Console
+        ## Iteration will be printed indicating which RSS filter is currently being processed
+      
+
+    ## Output in R environment
+        ## RSS.filters - dataframe that contains the overall localization error when RSS filters are applied prior to trilateration analysis
+            ## columns are the same as described in the no.filter table
+
+
+   ## Output saved
+        ## Trilateration.Simulation_Filters.RSS_Summary.Stats.csv - .csv file of the same dataframe returned in the R environment 
+        ## _Filters.RSS_Summary.Results.csv - 4 dataframes - one .csv file for each RSS filter that has the summary statistics of localization error 
+                                                ## when the specified filter was applied prior to trilateration. Each row represents the summary  
+                                                ## statistics for all simulations for a given random point. Files are for your reference. 
+
+
+
+
+##****** Define variables - indicate the RSS values to filter data prior to trilateration ************#
+RSS.FILTER <- c(-80, -85, -90, -95)
+
+
+# Estimate locations of 100 random points when RSS filters are applied prior to trilateration 
+RSS.filters <- trilateration.RSS.Filter(random.rss_results.all.sim)
+
+
+
+
+
+
+###########################################################################################
+# Trilateration - Distance from strongest node filter 
+    # Function that will estimate the location of each random point
+    # in the simulated dataset when distance filters are applied prior to trilateration 
+###########################################################################################
+
+
+    ## Output in Console
+        ## Iteration will be printed indicating which distance filter is currently being processed
+
+
+    ## Output in R environment
+        ## Dist.filters - dataframe that contains the overall localization error when distance filters are applied prior to trilateration analysis
+            ## columns are the same as described in the no.filter table
+
+
+   ## Output saved
+        ## Trilateration.Simulation_Filters.Distance_Summary.Stats.csv - .csv file of the same dataframe returned in the R environment 
+        ## Filters.Distance_Summary.Results.csv - 4 dataframes - .csv file for each Distance filter that has the summary statistics of localization error 
+                                                                ## when the specified filter was applied prior to trilateration. Each row represents the summary 
+                                                                ## statistics for all simulations for a given random point. Files are for your reference. 
+
+
+
+
+# Estimate locations of 100 random points when distance filters are applied prior to trilateration 
+    # Distance filter was created in previous step when you defined the node configuration
+        # DIST.FILTER = distances that are a multiple (x1.25, x2, x3, x4) of the average grid spacing
+            # For each random test point the node with the strongest signal is identified and then only nodes
+            # within the specified distance from the strongest node are kept for trilateration analysis
+Dist.filter <- trilateration.Distance.Filter(random.rss_results.all.sim)
+
+
+
+
+
